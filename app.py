73d83416f9a2870b24afbd4e77ec7bb3ac1163c1
Ayman <aymanrafid1905@gmail.com>
2026-05-03 18:55:38 +0800

    Login route p1

--- app.py
+++ app.py
@@ -8,7 +8,12 @@ app.secret_key = "your_secret_key"  # Needed for session management
 def home():
     return "Welcome to the Login Page! <a href='/login'>Go to Login</a>"
 
-
+# Login route
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
 
 if __name__ == '__main__':
     app.run(debug=True)
