f49fd29b53b8ba431a48173470dc99794973aa72
Ayman <aymanrafid1905@gmail.com>
2026-05-03 18:54:51 +0800

    Deleted login route

--- app.py
+++ app.py
@@ -8,16 +8,7 @@ app.secret_key = "your_secret_key"  # Needed for session management
 def home():
     return "Welcome to the Login Page! <a href='/login'>Go to Login</a>"
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        if username == 'admin' and password == '1234':
-            return "Login successful!"
-        else:
-            return "Invalid credentials"
-    return render_template('login.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
