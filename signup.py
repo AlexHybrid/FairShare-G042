3096d877a054bb1ef63f90c0f692feb59ac388a1
Ayman <aymanrafid1905@gmail.com>
2026-05-03 23:15:42 +0800

    signup login

--- signup.py
+++ signup.py
@@ -8,3 +8,19 @@ users = {}
 @app.route('/')
 def home():
     return redirect(url_for('login'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+
+        # Check if user exists and password matches
+        if username in users and users[username]['password'] == password:
+            return f"Welcome, {username}!"
+        else:
+            error = "Invalid username or password"
+            return render_template('login.html', error=error)
+
+    return render_template('login.html')
