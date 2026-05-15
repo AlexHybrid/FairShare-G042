e1770eb1b7fb60fab80647e7b02f95c95e4f10e1
Alivs <muhdalifnazri07@gmail.com>
2026-05-15 23:36:57 +0800

    data bridge

--- pdfExport.py
+++ pdfExport.py
@@ -4,3 +4,15 @@ from reportlab.lib import colors
 import os
 
 export = Flask(__name__)
+
+#---Dynamic Data Bridge---
+def get_user_data():
+    # Simulating 'live' data pulled from the FairShare database
+    live_data = [
+        ["Date", "Room", "Name", "Amount"],
+        ["15 May 2026", "Room A", "Muhammad Alif", "$150"],
+        ["15 May 2026", "Room A", "Housemate 1", "$50"],
+        ["16 May 2026", "Room C", "Housemate 2", "$100"]
+    ]
+    return live_data
+
