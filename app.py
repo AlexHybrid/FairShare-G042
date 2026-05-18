2fd99c98505a0d76b96b087ccb3315259a18d696
ayman <aymanrafid1905@gmail.com>
2026-05-18 21:15:23 +0800

    Pip problem fixed

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
-from flask import Flask, render_template, request, redirect, url_for, session, flash
+from flask import Flask, render_template, request, redirect, url_for, session, flash 
 import sqlite3
 from werkzeug.security import generate_password_hash, check_password_hash
