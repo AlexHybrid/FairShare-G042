25973e1af80d4a792eede9c967893e901dec14da
ayman <aymanrafid1905@gmail.com>
2026-05-18 11:26:28 +0800

    File modification

---
+++ app.py
@@ -0,0 +1,5 @@
+import os
+from flask import Flask, render_template, redirect, url_for, flash, request
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
+from models import db, User
