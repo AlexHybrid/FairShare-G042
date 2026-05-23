064671ab5d9dab09c458d37014ef581c8348c6d8
ayman <aymanrafid1905@gmail.com>
2026-05-23 22:16:26 +0800

    Database Creating

--- init_db.py
+++ init_db.py
@@ -1,2 +1,8 @@
 import sqlite3
 
+# This script creates our SQLite database and the users table.
+# Run this file once before starting the Flask app.
+
+def create_db():
+    # Connect to the database (it will be created if it doesn't exist)
+    connection = sqlite3.connect('users.db')
